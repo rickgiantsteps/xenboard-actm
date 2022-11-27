48a031c10495069931289a62f58e1081fef5cddf
Riccardo Passoni <riccardopassoni@protonmail.com>
2022-11-27 13:23:28 +0100

    added dynamic oscillators

--- src/components/XenBoard.vue
+++ src/components/XenBoard.vue
@@ -3,45 +3,66 @@
     <h1>XenBoard</h1>
     <p>Microtonal keyboard test</p>
   </header>
+  <label>Number of notes: </label>
   <input
       type="number"
       id="hexnum"
       name="hexnum"
       v-model.number = "hexNumber"
       min="1"
-      max="100" />
+      max="100"
+      v-on:change="createOsc(hexNumber)"
+      />
 
   <div class="grid" id='hexgrid'>
-    <HexagonKey v-bind:key="n" v-text="n-1" v-bind:id="n" v-for="n in hexNumber"></HexagonKey>
+    <HexagonKey @click="playOscillator(n)" v-bind:key="n" v-text="n-1" v-bind:id="n" v-for="n in hexNumber"></HexagonKey>
   </div>
 
   <p>ACTM Project - a.a. 2022/2023</p>
 </template>
 
 <script>
+import * as Tone from 'tone'
 import HexagonKey from './hex.vue'
 
-//@Component({ components: { hexagonKey } })
+let synth = new Array(12)
+let note = new Array(12)
+let octave = 0;
+
+for (let i=0; i<=12; i++) {
+  synth[i] = new Tone.Synth().toDestination();
+  note[i] = 440 * 2 ** ((i + (octave * 12)) / 12)
+}
 
 export default {
+
   data() {
     return {
-      hexNumber: '',
+      hexNumber: 12,
     };
   },
 
-  beforeMount: function() {
-    this.hexNumber = 12;
+  methods: {
+    createOsc(hexNumber) {
+      for (let i=0; i<=hexNumber; i++) {
+        synth[i] = new Tone.Synth().toDestination();
+        note[i] = 440 * 2**((i + (octave*hexNumber))/hexNumber)
+      }
+    },
+
+    playOscillator(n) {
+      synth[n].triggerAttackRelease(note[n], "8n");
+    }
   },
 
   name: 'XenBoard',
   props: {
+
   },
 
-components: {
-    HexagonKey
-}
-
+  components: {
+      HexagonKey
+  }
 
 }
 </script>
