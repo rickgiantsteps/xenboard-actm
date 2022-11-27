4402873344866354d8f29bb455e73c478b83b089
Riccardo Passoni <riccardopassoni@protonmail.com>
2022-11-27 16:42:41 +0100

    added octave, freq and root controls

--- src/components/XenBoard.vue
+++ src/components/XenBoard.vue
@@ -3,23 +3,31 @@
     <h1>XenBoard</h1>
     <p>Microtonal keyboard test</p>
   </header>
+
+  <label>Central frequency (Hz): </label>
+  <input type="number" id="freqhz" name="freqhz"
+         v-model.number = "centerfreq" min="1" v-on:change="createOsc(hexNumber, octaves, centerfreq, rootn)"/>
+
   <label>Number of notes: </label>
-  <input
-      type="number"
-      id="hexnum"
-      name="hexnum"
-      v-model.number = "hexNumber"
-      min="1"
-      max="100"
-      v-on:change="createOsc(hexNumber)"
-      />
+  <input type="number" id="hexnum" name="hexnum"
+         v-model.number = "hexNumber" min="1" max="100" v-on:change="createOsc(hexNumber, octaves, centerfreq, rootn)"/>
+
+  <label>Number of octaves: </label>
+  <input type="number" id="octnum" name="octnum"
+         v-model.number = "octaves" min="1" max="10" v-on:change="createOsc(hexNumber, octaves, centerfreq, rootn)"/>
+
+  <label>Root: </label>
+  <input type="number" id="root" name="root"
+         v-model.number = "rootn" min="2" max="5" v-on:change="createOsc(hexNumber, octaves, centerfreq, rootn)"/>
 
   <div class="grid" id='hexgrid'>
-    <HexagonKey @click="playOscillator(n)" v-bind:key="n" v-text="n-1" v-bind:id="n" v-for="n in hexNumber"></HexagonKey>
+    <HexagonKey @click="playOscillator(n-1)" v-bind:key="n" v-text="n-1" v-bind:id="n" v-for="n in hexNumber*octaves"/>
   </div>
 
   <p>ACTM Project - a.a. 2022/2023</p>
 </template>
+
+
 
 <script>
 import * as Tone from 'tone'
@@ -35,18 +43,27 @@ for (let i=0; i<=12; i++) {
 }
 
 export default {
+  name: 'XenBoard',
 
   data() {
     return {
       hexNumber: 12,
+      octaves: 1,
+      centerfreq: 440,
+      rootn: 2,
     };
   },
 
   methods: {
-    createOsc(hexNumber) {
-      for (let i=0; i<=hexNumber; i++) {
-        synth[i] = new Tone.Synth().toDestination();
-        note[i] = 440 * 2**((i + (octave*hexNumber))/hexNumber)
+    createOsc(hexNumber, octaves, freq, root) {
+    for (let j=0; j<octaves; j++) {
+      synth.length = hexNumber * octaves
+      note.length = hexNumber * octaves
+        for (let i = hexNumber * j ; i<hexNumber*(j+1); i++) {
+          synth[i] = new Tone.Synth().toDestination();
+          note[i] = (freq * root**j) * root**((i%hexNumber)/hexNumber)
+        }
+        console.log(note)
       }
     },
 
@@ -55,9 +72,7 @@ export default {
     }
   },
 
-  name: 'XenBoard',
   props: {
-
   },
 
   components: {
